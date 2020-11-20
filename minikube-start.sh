bf91b47cef9cbf7f24c1020e3b2884080eb91939
Steve Moyer <smoyer1@selesy.com>
2020-11-20 15:41:13 -0500

    Moves manifests to appropriate k8s subfolders

--- minikube-start.sh
+++ minikube-start.sh
@@ -3,5 +3,5 @@
 minikube start --cpus=2 --memory=4G --addons=ingress
 eval $(minikube -p minikube docker-env)
 
-kubectl apply -f k8s/coredns.yaml
-kubectl apply -f k8s/rook-cockroachdb-operator.yaml
+kubectl apply -f k8s/minikube/coredns.yaml
+kubectl apply -f k8s/minikube/rook-cockroachdb-operator.yaml
